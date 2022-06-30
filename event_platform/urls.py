095bd48681123a4de762ea05737ac155a6b1b738
isaquesousa2 <isaqueleal35@gmail.com>
2022-06-30 01:14:31 -0300

    up

--- event_platform/urls.py
+++ event_platform/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from lessons.urls import router
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
