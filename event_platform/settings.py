a73636b3b860fb21bebbcba5bd9b86f717fefdd5
isaquesousa2 <isaqueleal35@gmail.com>
2022-06-30 00:24:25 -0300

    up

--- event_platform/settings.py
+++ event_platform/settings.py
@@ -105,7 +105,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
